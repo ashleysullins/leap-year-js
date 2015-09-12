89680b70956520018d5328515d0fff46bae93f39
the holla <holla@bikefarm.org>
2015-09-12 16:45:14 -0700

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) { // name of form
+    var year = parseInt($("input#year").val()); // val() gets input from form
+    var result = leapYear(year);
+
+    $(".year").text(year); // .year from span class year
+    if (!result) {
+      $(".not").text("not"); // .not from span class not
+    }
+
+    $("#result").show(); // shows the div id that we hid in css
+    event.preventDefault(); // make sure it doesn't submit to a server
+  });
+});
---
+++ spec/specs.js
@@ -0,0 +1,13 @@
+describe('leapYear', function() { //first arg is a sting and the name of the function to be built
+  it("is false for a year that is not divisible by 4", function() { //first arg is string descrip of behavior
+    expect(leapYear(1999)).to.equal(false);
+  });
+
+  it("is true for most years divisible by 4", function() {
+    expect(leapYear(2012)).to.equal(true);
+  });
+
+  it("is false for most years divisible by 100", function() {
+    expect(leapYear(1900)).to.equal(false);
+  });
+});
